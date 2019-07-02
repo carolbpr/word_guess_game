2ab21a1bc99a4d19207d8b88222c951783b22276
Caro Berry <carolbpr@gmail.com>
2019-07-02 16:01:15 -0500

    touchend function added for mobile

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -137,7 +137,89 @@ var usastates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colo
 
 // When user press a key this function is run.
 initgame();
+document.ontouchend = function(event){
+   // Determines which key was pressed.  
+    initialMessagetext.textContent = " ";
+    answertext.textContent = " ";
+    userGuess = event.key;
+    var letterfound = false;
+    var keyCode = event.which;
 
+    //This conditions determinate either if the letter entered has been already entered, if it is a letter or not, of if it is a new letter
+    if (alredyguessedletter.includes(userGuess)) {
+        document.getElementById('nope-sound').play();
+        alert("Choose another letter, you already guessed this");
+    }
+    else if (keyCode < 65 || keyCode > 90) {
+        document.getElementById('nope-sound').play();
+        alert("Please choose just letters");
+    }
+
+    //if it is a new letter then these conditions will identify if the letter is part of the word or not
+    else {
+        document.getElementById('click-sound').play();
+        alredyguessedletter.push(userGuess);
+        for (var j = 0; j < wordlowercase.length; j++) {
+
+            //This condition will fill the answer array with the letters that are part of the word
+            if (userGuess.toUpperCase() === string[j]) {
+                answerArray[j] = userGuess.toUpperCase();
+                countercomplete++;
+                letterfound = true;
+
+                //This condition will check if the word is completed
+                if (countercomplete === wordselected.length) {
+                    
+                    complete = true;
+                    //will play a Winner sound
+                    document.getElementById('winner-sound').play();
+                    answertext.textContent = "Congrats! The answer is " + wordselected;
+                    wins++
+                    findimage();
+
+                    //This will show a popup window with the correct answer guessed and a Image of the State
+                    modal.style.display = "block";
+                    imageanwser.src = states[stateimage];
+                    captionText.textContent = wordselected;
+                    initgame();
+
+                    // Get the <span> element that closes the modal
+                    var span = document.getElementsByClassName("close")[0];
+
+                    // When the user clicks on <span> (x), close the modal
+                    span.onclick = function () {
+                        modal.style.display = "none";
+                        
+                    }
+                }
+            }
+        }
+
+        //This condition will fill the array with the wrong letters
+        if (letterfound === false && complete === false) {
+            remainingLetters--;
+            wrongletter.push(userGuess.toUpperCase());
+
+            //This condition will check if the user has remaining time to guess 
+            if (remainingLetters === 0) {
+                youlost = true;
+                document.getElementById('lost-sound').play();
+                alert("Sorry! You lost. Try again !")
+                initgame()
+            }
+        }
+
+    }
+
+    winstext.textContent = wins;
+    //Replace "," to " "(spaces)
+    currentwordtext.textContent = answerArray.toString().replace(new RegExp(",", 'g'), " ");
+    remainingguessestext.textContent = remainingLetters;
+    //Replace "," to " "(spaces)
+    letterguessestext.textContent = wrongletter.toString().replace(new RegExp(",", 'g'), " ");
+
+} 
+}
 document.onkeyup = function (event) {
     // Determines which key was pressed.  
     initialMessagetext.textContent = " ";
